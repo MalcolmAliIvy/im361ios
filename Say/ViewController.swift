c219b1d152143292911a8c18d617a952a3ce135e
Malcolm <malcolmivy7@gmail.com>
2019-04-09 23:45:24 -0500

    Initial Commit

---
+++ Say/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Say
+//
+//  Created by Malcolm Ivy on 4/9/19.
+//  Copyright © 2019 Malcolm Ivy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SayTests/SayTests.swift
@@ -0,0 +1,34 @@
+//
+//  SayTests.swift
+//  SayTests
+//
+//  Created by Malcolm Ivy on 4/9/19.
+//  Copyright © 2019 Malcolm Ivy. All rights reserved.
+//
+
+import XCTest
+@testable import Say
+
+class SayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
